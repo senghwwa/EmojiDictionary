04e0e75127c04b80fd6c258677a44948dbed5b65
Seng Hwwa <senghwwa@zana-girl.local>
2018-12-28 12:28:32 +0800

    All done. Old class and variable cleaned up

--- EmojiDictionary/Emoji.swift
+++ EmojiDictionary/Emoji.swift
@@ -8,21 +8,7 @@
 
 import Foundation
 
-class Emoji {
-    var symbol: String
-    var name: String
-    var description: String
-    var usage: String
-    
-    init(symbol: String, name: String, description: String, usage: String) {
-        self.symbol = symbol
-        self.name = name
-        self.description = description
-        self.usage = usage
-    }
-}
-
-struct Emoji1 {
+struct Emoji {
     var symbol: String
     var name: String
     var description: String
@@ -31,63 +17,48 @@ struct Emoji1 {
 
 struct EmojiGrouping {
     var groupName: String
-    var emojis: [Emoji1]
+    var emojis: [Emoji]
 }
 
 var emojiGrouping: [EmojiGrouping] = []
 
-var xEmojis = [Emoji1]()
+var emojis = [Emoji]()
 
 
 func loadEmojiData() {
-    xEmojis.append(Emoji1(symbol: "😀", name: "Grinning Face", description: "A typical smiley face.", usage: "happiness"))
-    xEmojis.append(Emoji1(symbol: "😕", name: "Confused Face", description: "A confused, puzzled face.", usage: "unsure what to think; displeasure"))
-    xEmojis.append(Emoji1(symbol: "😍", name: "Heart Eyes",description: "A smiley face with hearts for eyes.", usage: "love of something; attractive"))
-    xEmojis.append(Emoji1(symbol: "👮", name: "Police Officer", description: "A police officer wearing a blue cap with a gold badge.", usage: "person of authority"))
-    emojiGrouping.append(EmojiGrouping(groupName: "Smileys & People", emojis: xEmojis))
+    emojis.append(Emoji(symbol: "😀", name: "Grinning Face", description: "A typical smiley face.", usage: "happiness"))
+    emojis.append(Emoji(symbol: "😕", name: "Confused Face", description: "A confused, puzzled face.", usage: "unsure what to think; displeasure"))
+    emojis.append(Emoji(symbol: "😍", name: "Heart Eyes",description: "A smiley face with hearts for eyes.", usage: "love of something; attractive"))
+    emojis.append(Emoji(symbol: "👮", name: "Police Officer", description: "A police officer wearing a blue cap with a gold badge.", usage: "person of authority"))
+    emojiGrouping.append(EmojiGrouping(groupName: "Smileys & People", emojis: emojis))
     
-    xEmojis.removeAll()
-    xEmojis.append(Emoji1(symbol: "🐢", name: "Turtle", description: "A cute turtle.", usage: "Something slow"))
-    xEmojis.append(Emoji1(symbol: "🐘", name: "Elephant", description: "A gray elephant.", usage: "good memory"))
-    emojiGrouping.append(EmojiGrouping(groupName: "Animals & Nature", emojis: xEmojis))
+    emojis.removeAll()
+    emojis.append(Emoji(symbol: "🐢", name: "Turtle", description: "A cute turtle.", usage: "Something slow"))
+    emojis.append(Emoji(symbol: "🐘", name: "Elephant", description: "A gray elephant.", usage: "good memory"))
+    emojiGrouping.append(EmojiGrouping(groupName: "Animals & Nature", emojis: emojis))
     
-    xEmojis.removeAll()
-    xEmojis.append(Emoji1(symbol: "🍝", name: "Spaghetti", description: "A plate of spaghetti.", usage: "spaghetti"))
-    emojiGrouping.append(EmojiGrouping(groupName: "Food & Drink", emojis: xEmojis))
+    emojis.removeAll()
+    emojis.append(Emoji(symbol: "🍝", name: "Spaghetti", description: "A plate of spaghetti.", usage: "spaghetti"))
+    emojiGrouping.append(EmojiGrouping(groupName: "Food & Drink", emojis: emojis))
     
-    xEmojis.removeAll()
-    xEmojis.append(Emoji1(symbol: "🎲", name: "Die", description: "A single die.", usage: "taking a risk, chance; game"))
-    xEmojis.append(Emoji1(symbol: "⛺️", name: "Tent", description: "A small tent.", usage: "camping"))
-    emojiGrouping.append(EmojiGrouping(groupName: "Activity", emojis: xEmojis))
+    emojis.removeAll()
+    emojis.append(Emoji(symbol: "🎲", name: "Die", description: "A single die.", usage: "taking a risk, chance; game"))
+    emojis.append(Emoji(symbol: "⛺️", name: "Tent", description: "A small tent.", usage: "camping"))
+    emojiGrouping.append(EmojiGrouping(groupName: "Activity", emojis: emojis))
     
-    xEmojis.removeAll()
-    xEmojis.append(Emoji1(symbol: "📚", name: "Stack of Books", description: "Three colored books stacked on each other.", usage: "homework, studying"))
-    emojiGrouping.append(EmojiGrouping(groupName: "Objects", emojis: xEmojis))
+    emojis.removeAll()
+    emojis.append(Emoji(symbol: "📚", name: "Stack of Books", description: "Three colored books stacked on each other.", usage: "homework, studying"))
+    emojiGrouping.append(EmojiGrouping(groupName: "Objects", emojis: emojis))
     
-    xEmojis.removeAll()
-    xEmojis.append(Emoji1(symbol: "💔", name: "Broken Heart", description: "A red, broken heart.", usage: "extreme sadness"))
-    xEmojis.append(Emoji1(symbol: "💤", name: "Snore", description: "Three blue \'z\'s.", usage: "tired, sleepiness"))
-    emojiGrouping.append(EmojiGrouping(groupName: "Symbols", emojis: xEmojis))
+    emojis.removeAll()
+    emojis.append(Emoji(symbol: "💔", name: "Broken Heart", description: "A red, broken heart.", usage: "extreme sadness"))
+    emojis.append(Emoji(symbol: "💤", name: "Snore", description: "Three blue \'z\'s.", usage: "tired, sleepiness"))
+    emojiGrouping.append(EmojiGrouping(groupName: "Symbols", emojis: emojis))
     
-    xEmojis.removeAll()
-    xEmojis.append(Emoji1(symbol: "🏁", name: "Checkered Flag", description: "A black-and-white checkered flag.", usage: "completion"))
-    emojiGrouping.append(EmojiGrouping(groupName: "Flags", emojis: xEmojis))
+    emojis.removeAll()
+    emojis.append(Emoji(symbol: "🏁", name: "Checkered Flag", description: "A black-and-white checkered flag.", usage: "completion"))
+    emojiGrouping.append(EmojiGrouping(groupName: "Flags", emojis: emojis))
 }
 
-//
 
-//Objects
-//Symbols
-//Flags
-
-//
-//,
-//,
-//,
-//,
-//,
-//,
-//,
-//,
-//]
 
--- EmojiDictionary/EmojiTableViewController.swift
+++ EmojiDictionary/EmojiTableViewController.swift
@@ -10,34 +10,6 @@ import UIKit
 
 class EmojiTableViewController: UITableViewController {
   
-    
-    var oldemojis: [Emoji] = [
-        Emoji(symbol: "😀", name: "Grinning Face",
-              description: "A typical smiley face.", usage: "happiness"),
-        Emoji(symbol: "😕", name: "Confused Face",
-              description: "A confused, puzzled face.", usage: "unsure what to think; displeasure"),
-        Emoji(symbol: "😍", name: "Heart Eyes",
-              description: "A smiley face with hearts for eyes.",
-              usage: "love of something; attractive"),
-        Emoji(symbol: "👮", name: "Police Officer",
-              description: "A police officer wearing a blue cap with a gold badge.", usage: "person of authority"),
-        Emoji(symbol: "🐢", name: "Turtle", description:
-            "A cute turtle.", usage: "Something slow"),
-        Emoji(symbol: "🐘", name: "Elephant", description:
-            "A gray elephant.", usage: "good memory"),
-        Emoji(symbol: "🍝", name: "Spaghetti",
-              description: "A plate of spaghetti.", usage: "spaghetti"),
-        Emoji(symbol: "🎲", name: "Die", description: "A single die.", usage: "taking a risk, chance; game"),
-        Emoji(symbol: "⛺️", name: "Tent", description: "A small tent.", usage: "camping"),
-        Emoji(symbol: "📚", name: "Stack of Books",
-              description: "Three colored books stacked on each other.",
-              usage: "homework, studying"),
-        Emoji(symbol: "💔", name: "Broken Heart",
-              description: "A red, broken heart.", usage: "extreme sadness"),
-        Emoji(symbol: "💤", name: "Snore", description: "Three blue \'z\'s.", usage: "tired, sleepiness"),
-        Emoji(symbol: "🏁", name: "Checkered Flag", description: "A black-and-white checkered flag.", usage: "completion")]
- 
-    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -53,7 +25,6 @@ class EmojiTableViewController: UITableViewController {
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
     // MARK: - Table view data source
@@ -75,6 +46,7 @@ class EmojiTableViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell:UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "EmojiCell", for: indexPath)
+        
 //        let emoji = emojis[indexPath.row]
 //        cell.textLabel?.text = "\(emoji.symbol) - \(emoji.name)"
 //        cell.detailTextLabel?.text = emoji.description
@@ -94,10 +66,11 @@ class EmojiTableViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //let emoji = emojis[indexPath.row]
+
         let emojiGroup = emojiGrouping[indexPath.section]
         let emoji = emojiGroup.emojis[indexPath.row]
         
-        //let emoji = emojis[indexPath.row]
         print("\(emoji.symbol) \(indexPath)")
     }
     
@@ -126,6 +99,7 @@ class EmojiTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
 //        let movedEmoji = emojis.remove(at: fromIndexPath.row)
 //        emojis.insert(movedEmoji, at: to.row)
+
         print("Section is \(fromIndexPath.section). From row is \(fromIndexPath.row). To row is \(to.section):\(to.row)")
         
         if fromIndexPath.section == to.section {
